b4340fc7e15c7ff28d931a56a74ce8a45253bd99
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-02-22 00:10:03 -0800

    Working adding/equipping cards
    some issues in using cards correctly, specifically with cooldowns and initial values. pretty sure its an issue in the initial values

---
+++ Assets/CardBackend/MultiuseCardModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CardOperations
+{
+    [CreateAssetMenu(menuName = "Card Models/Multi-Use Cards")]
+    /// <summary>
+    /// Model for cards which can be used multiple times before being put on cooldown
+    /// </summary>
+    public class MultiuseCardModel : CooldownCardModel
+    {
+        public int maxUses;
+        private int remainingUses;
+
+        public new void Awake()
+        {
+            Debug.Log("multiuse card model awake: " + cardName);
+            remainingUses = maxUses;
+            base.Awake();
+        }
+
+        // TODO fix UseActive conditions 
+        public override void UseActive()
+        {
+            // first checks if remaining uses
+            if (remainingUses > 0)
+            {
+                Debug.Log("Card " + name + " has no uses left");
+                return;
+                // ability not used if on cooldown
+            }
+            
+            // counts down remaining uses, then uses ability
+            remainingUses -= 1;
+            Debug.Log("Card " + name + " has uses left " + remainingUses);
+            // if ran out of uses, start card cooldown.
+            if (remainingUses == 0) { StartCooldown(); }
+            base.UseActive();
+        }
+        public override void SwapIn()
+        {
+
+        }
+        public override void SwapOut()
+        {
+
+        }
+    }
+}
