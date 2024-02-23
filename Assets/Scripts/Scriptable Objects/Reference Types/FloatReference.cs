c31ec4ea3963c43bca0cb547c7540eaecb89b12a
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-02-22 20:34:32 -0800

    Make private members protected for inheritance

--- Assets/Scripts/Scriptable Objects/Reference Types/FloatReference.cs
+++ Assets/Scripts/Scriptable Objects/Reference Types/FloatReference.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Reference Types/Float")]
 public class FloatReference : ScriptableObject
 {
-    private float value;
-    private float defaultValue;
+    protected float value;
+    protected float defaultValue;
     public float Value { get; set; }
     public float DefaultValue => defaultValue;
 
