81d8fd47a1bef434ed3d431352dae548c6c23622
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-03-05 23:17:06 -0800

    nevermind im going to sleep
    giving up for tonight. framework for adding abilities is set up, just need to do the awkward stuff to piece everything together, which will probably have to be done in main

--- Assets/Scripts/Ability_CardBackend/AbilityScripts/AbilityHolder.cs
+++ Assets/Scripts/Ability_CardBackend/AbilityScripts/AbilityHolder.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// will do later
+
 public class AbilityHolder : MonoBehaviour
 {
-    
+    public BaseAbility PrimaryAbility;
+    public BaseAbility SecondaryAbility;
+    public BaseAbility SpecialAbility;
+    public BaseAbility MovementAbility;
     // Start is called before the first frame update
     void Start()
     {
