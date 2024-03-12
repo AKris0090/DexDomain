be86720d81774cc401366d288f60f2870721dc69
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-03-11 21:05:25 -0700

    ui system working properly
    UI will now swap cards with equipped cards properly

--- Assets/Scripts/UIScripts/AbilityManager.cs
+++ Assets/Scripts/UIScripts/AbilityManager.cs
@@ -26,6 +26,8 @@ public class AbilityManager : MonoBehaviour
 
 
     public List<Canvas> abilitySlots;
+    Dictionary<BaseCardClass, GameObject> cardToUI;
+    Dictionary<GameObject, BaseCardClass> UIToCard;
 
     public List<Canvas> deckSlots;
     private int nextSlot = 0;
@@ -33,6 +35,20 @@ public class AbilityManager : MonoBehaviour
 
     void Start()
     {
+        cardToUI = new Dictionary<BaseCardClass, GameObject>();
+        UIToCard = new Dictionary<GameObject, BaseCardClass>();
+    }
+
+    int findNextSlot()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            if (deckSlots[i].transform.childCount == 0)
+            {
+                return i;
+            }
+        }
+        return 0;
     }
 
     // add card to swapper page (called from Card Manager)
@@ -41,9 +57,19 @@ public class AbilityManager : MonoBehaviour
         Debug.Log("(UI) Adding " + card.cardName + " card to hand");
 
         // make visible
+        nextSlot = findNextSlot();
         GameObject newCard = Instantiate(card.cardUIPrefab, deckSlots[nextSlot].transform, false);
 
         // make obtainable (can move from hand to equipped)
+        if (cardToUI.ContainsKey(card))
+        {
+            cardToUI[card] = newCard;
+        }
+        else
+        {
+            cardToUI.Add(card, newCard);
+        }
+        Debug.Log(newCard);
         Button cardButton = deckSlots[nextSlot].GetComponentInChildren<Button>();
         cardButton.onClick.AddListener(() => CardManager.Instance.SwapIn(card));
         // basically clicking a card calls the card manager to do it's swapping in
@@ -65,6 +91,11 @@ public class AbilityManager : MonoBehaviour
         {
             foreach (Transform child in targetCanvas.transform)
             {
+                CardComponent test = child.GetComponent<CardComponent>();
+                if(test != null)
+                {
+                    AddCardToHand(test.CardScriptObj);
+                }
                 Destroy(child.gameObject);
             }
         }
@@ -72,5 +103,6 @@ public class AbilityManager : MonoBehaviour
         // add to equip slot
         Instantiate(card.cardUIPrefab, targetCanvas.transform, false);
         // using the cards should be done in player control
+        Destroy(cardToUI[card]);
     }
 }
