892ce17d81bd737b4102ca3a858225dd9ee72392
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-03-12 19:59:38 -0700

    boss test working

--- Assets/Scripts/Enemies/BossHealthBar.cs
+++ Assets/Scripts/Enemies/BossHealthBar.cs
@@ -26,6 +26,7 @@ public class BossHealthBar : MonoBehaviour
 
     public void Activate(int initialHealth)
     {
+        Debug.Log("hm");
         holder.SetActive(true);
         bar.fillAmount = 1;
         maxHealth = initialHealth;
